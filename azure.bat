38deedff980777956403e47c9b33d73f47ad5371
Pcego <pc-ego@users.noreply.github.com>
2020-09-18 10:57:18 +0200

    Update azure.bat

--- azure.bat
+++ azure.bat
@@ -3,3 +3,8 @@ cd zammad-docker-compose
 az group create --name zammad-group --location "West Europe"
 az appservice plan create --name zammad-plan --resource-group zammad-group --sku P1V2 --is-linux
 az webapp create --resource-group zammad-group --plan zammad-plan --name pcego-zammad --multicontainer-config-type compose --multicontainer-config-file docker-compose.yml
+# Attivare la registrazione del contenitore
+az webapp log config --name pcego-zammad --resource-group zammad-group --docker-container-logging filesystem
+# Abilitare lo streaming dei log
+az webapp log tail --name pcego-zammad --resource-group zammad-group
+#È anche possibile esaminare i file di log nel browser all'indirizzo https://pcego-zammad.scm.azurewebsites.net/api/logs/docker
